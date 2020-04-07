4af15b766fc083d16ecfd7be10f7de77aacabf56
mihai.g0g05arui <mihai.indreias@gmail.com>
2020-04-07 22:12:53 +0300

    Rezolvat bug de adaugat carti , se retin si dupa restart

--- lib/book_manager.dart
+++ lib/book_manager.dart
@@ -6,7 +6,13 @@ Book lastbook = new Book.fromEmpty();
 bookManager bm = new bookManager();
 List<Widget> listawid = new List<Widget>();
 List<Widget> listagata = new List<Widget>();
+int min(int a,int b){
+  if(a<b)
+    return a;
+  return b;
+}
 class bookManager{
+  int ad=0;
   List<Book>changed=new List<Book>();
   List<Book>finishedBooks=new List<Book>(),unfinishedBooks=new List<Book>();
   Book findByTitle(String title){
@@ -31,15 +37,19 @@ class bookManager{
   }
   void resetProgress(Book b){
       b.readPages=0;
+      flush();
   }
   void addProgress(Book b,int newPages){
-    b.readPages+=newPages;
+    b.readPages=min(b.pages,b.readPages+newPages);
     changed.add(b);
     flush();
   }
   void addBook(Book nb){
-    print(nb.name);
-    print(nb.author);
+    Book aux=findByTitle(nb.name);
+    if(aux.name==nb.name)
+      return;
+      ad++;
+    changed.add(nb);
     if(nb.pages==nb.readPages) {
       finishedBooks.add(nb);
       listagata.add(
@@ -65,13 +75,35 @@ class bookManager{
           print(unfinishedBooks[i].name);
       }
   }
-  void flush(){
-    for(int i=0;i<changed.length;i++){
-      Book cur=changed[i];
-      String titlu=cur.name,author=cur.author;
-      int id=cur.id,pages=cur.pages,readPages=cur.readPages;
-      FileUtils.saveToFile('books/$titlu.txt','$id\n"$titlu"\n"$author"\n$pages\n$readPages');
+  void flush()async {
+    for (int i = 0; i < changed.length; i++) {
+      Book cur = changed[i];
+      String titlu = cur.name,
+          author = cur.author;
+      int id = cur.id,
+          pages = cur.pages,
+          readPages = cur.readPages;
+      FileUtils.saveToFile(
+          'books/$titlu.txt', '$id\n"$titlu"\n"$author"\n$pages\n$readPages');
     }
     changed.clear();
+    if (ad >= 1) {
+      int nr = unfinishedBooks.length + finishedBooks.length;
+      String rez = "$nr\n";
+      for (int i = 0; i < unfinishedBooks.length; i++) {
+        String aux = unfinishedBooks[i].name;
+        rez += "\"$aux\"\n";
+      }
+      for (int i = 0; i < finishedBooks.length; i++) {
+        String aux = finishedBooks[i].name;
+        rez += "\"$aux\"\n";
+      }
+      saveText('books.txt', rez);
+      printAll();
+      print("AAAAAAAAA");
+      print(await FileUtils.readFromFile('books.txt'));
+      print(rez);
+      ad = 0;
+    }
   }
 }
